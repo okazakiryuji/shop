a277cc3e83e69d59c83527f3610e1a0202cd029a
EC2 Default User <ec2-user@ip-172-31-1-15.ap-northeast-1.compute.internal>
2022-12-21 06:55:19 +0000

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,36 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'sessions/new'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :public do
+    get 'sessions/new'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  devise_for :users
+  namespace :admin do
     get 'orders/show'
   end
   namespace :admin do
