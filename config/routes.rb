7aa83062c0d755d81b51aa65e892917977788a98
EC2 Default User <ec2-user@ip-172-31-1-15.ap-northeast-1.compute.internal>
2023-01-10 12:02:39 +0000

    adminログイン画面編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
+
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
-  namespace :admin do
-    get 'sessions/new'
-  end
+
   namespace :public do
     get 'addresses/index'
     get 'addresses/edit'
@@ -36,7 +35,6 @@ Rails.application.routes.draw do
     get 'homes/top'
     get 'homes/about'
   end
-  devise_for :users
   namespace :admin do
     get 'orders/show'
   end
