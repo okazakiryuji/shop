549e09b8e15431f9153399b2352969eb72796d70
EC2 Default User <ec2-user@ip-172-31-1-15.ap-northeast-1.compute.internal>
2023-01-30 14:50:12 +0000

    items/new作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,30 @@
 class Admin::ItemsController < ApplicationController
+
+  def new
+    @shop_image = ShopImage.new
+  end
+
+  def create
+    @shop_image = PostImage.new(shop_image_params)
+    @shop_image.user_id = current_user.id
+    @shop_image.save
+    redirect_to shop_images_path
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def post_image_params
+    params.require(:soph_image).permit(:items_name, :image, :caption)
+  end
+
 end
